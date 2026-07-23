349762bc082744e076a82ce311be8ceada8a460e
Ioana Marica <ioanamarica66@gmail.com>
2026-07-23 13:55:36 +0200

    Rewrite Spring App with Java-based configuration

--- spring-core/src/main/java/com/example/App.java
+++ spring-core/src/main/java/com/example/App.java
@@ -1,16 +1,19 @@
 package com.example;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@Configuration
+@ComponentScan(basePackages = "com.example")
 public class App 
 {
-    public static void main( String[] args )
-    {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Message message = context.getBean("messageID",Message.class);
-        message.display();
-        
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(Message.class);
+        Message message = context.getBean(Message.class);
+        System.out.println(message.display());
+
     }
 }
--- spring-core/src/main/java/com/example/Message.java
+++ spring-core/src/main/java/com/example/Message.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Message{
 
-    public void display(){
-        System.out.println("Hello Spring!");
+    @Bean
+    public String display(){
+        return "Hello, World!";
     }
 }
 
