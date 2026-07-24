bba3d64f4a058ade35a0afc5f5dd32e474b42532
Ioana Marica <ioanamarica66@gmail.com>
2026-07-24 08:56:19 +0200

    Add profile to pom and test printing for different profiles.

--- parent-project/src/main/java/com/baeldung/App.java
+++ parent-project/src/main/java/com/baeldung/App.java
@@ -1,10 +1,8 @@
 package com.baeldung;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Environment: " + System.getProperty("env"));
+  }
 }
+
