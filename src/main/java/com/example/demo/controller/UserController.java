757f427a8a5bc756a4ed1dac5bac39a00b463da0
Ioana Marica <ioanamarica66@gmail.com>
2026-07-24 11:04:12 +0200

    Solve exercises 2.1 - 2.2, add logging statements using slf4j in DemoApp and UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,18 @@ import com.example.demo.service.UserService;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
+        logger.info("UserController initialized");
         this.userService = userService;
     }
 
     @GetMapping
     public List<User> getAllUsers() {
+        logger.info("Fetching all users");
         return userService.getAll();
     }
 }
