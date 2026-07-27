3410f543179de6b70e0d699ff81279450b3c6f48
Ioana Marica <ioanamarica66@gmail.com>
2026-07-27 08:49:19 +0200

    Fix test for getUsersByName

--- spring-boot/src/test/java/com/example/spring_project/UserControllerEndpointsTest.java
+++ spring-boot/src/test/java/com/example/spring_project/UserControllerEndpointsTest.java
@@ -26,84 +26,88 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(UserController.class)
 public class UserControllerEndpointsTest {
 
-   @MockitoBean
-   private UserServiceImpl userService;
+      @MockitoBean
+      private UserServiceImpl userService;
 
-   @Autowired
-   private MockMvc mvc;
+      @Autowired
+      private MockMvc mvc;
 
-   @Test
-   public void testShowUsersEndpoint() throws Exception {
-      when(userService.getAll()).thenReturn(Collections.emptyList());
-      mvc.perform(get("/users"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
-      verify(userService).getAll();
-   }
+      @Test
+      public void testShowUsersEndpoint() throws Exception {
+            when(userService.getAll()).thenReturn(Collections.emptyList());
+            mvc.perform(get("/users"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+            verify(userService).getAll();
+      }
 
-   @Test
-   public void testAddUserEndpoint() throws Exception {
-      User user = new User(1, "John Doe", 30);
-      when(userService.saveEntity(any(User.class)))
-            .thenReturn(user);
-      mvc.perform(post("/users")
-            .contentType(APPLICATION_JSON)
-            .content("""
-                  {
-                      "id":1,
-                      "name":"John Doe",
-                      "age":30
-                  }
-                  """))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
-      verify(userService).saveEntity(any(User.class));
-   }
+      @Test
+      public void testAddUserEndpoint() throws Exception {
+            User user = new User(1, "John Doe", 30);
+            when(userService.saveEntity(any(User.class)))
+                        .thenReturn(user);
+            mvc.perform(post("/users")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+                                    {
+                                        "id":1,
+                                        "name":"John Doe",
+                                        "age":30
+                                    }
+                                    """))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+            verify(userService).saveEntity(any(User.class));
+      }
 
-   @Test
-   public void testDeleteUserEndpoint() throws Exception {
-      doNothing().when(userService).deleteEntity(any());
-      mvc.perform(delete("/users/1"))
-            .andExpect(status().isNoContent());
-      verify(userService).deleteEntity(any());
-   }
+      @Test
+      public void testDeleteUserEndpoint() throws Exception {
+            doNothing().when(userService).deleteEntity(any());
+            mvc.perform(delete("/users/1"))
+                        .andExpect(status().isNoContent());
+            verify(userService).deleteEntity(any());
+      }
 
-   @Test
-   public void testUpdateUserEndpoint() throws Exception {
-      User user = new User(1, "John Doe", 30);
-      when(userService.updateEntity(any(User.class)))
-            .thenReturn(user);
-      mvc.perform(put("/users/1")
-            .contentType(APPLICATION_JSON)
-            .content("""
-                  {
-                      "id":1,
-                      "name":"John Doe",
-                      "age":30
-                  }
-                  """))
-            .andExpect(status().isOk());
-      verify(userService).updateEntity(any(User.class));
-   }
+      @Test
+      public void testUpdateUserEndpoint() throws Exception {
+            User user = new User(1, "John Doe", 30);
+            when(userService.updateEntity(any(User.class)))
+                        .thenReturn(user);
+            mvc.perform(put("/users/1")
+                        .contentType(APPLICATION_JSON)
+                        .content("""
+                                    {
+                                        "id":1,
+                                        "name":"John Doe",
+                                        "age":30
+                                    }
+                                    """))
+                        .andExpect(status().isOk());
+            verify(userService).updateEntity(any(User.class));
+      }
 
-   @Test
-   public void testGetUserByIdEndpoint() throws Exception {
-      when(userService.getById(1))
-            .thenReturn(new User(1, "John Doe", 30));
-      mvc.perform(get("/users/1"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
-      verify(userService).getById(1);
-   }
+      @Test
+      public void testGetUserByIdEndpoint() throws Exception {
+            when(userService.getById(1))
+                        .thenReturn(new User(1, "John Doe", 30));
+            mvc.perform(get("/users/1"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+            verify(userService).getById(1);
+      }
 
-   @Test
-   public void testGetUsersByNameEndpoint() throws Exception {
-      when(userService.getByName("John"))
-            .thenReturn(Collections.singletonList(
-                  new User(1, "John Doe", 30)));
-      mvc.perform(get("/users/name/John"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
-      verify(userService).getByName("John");
-   }
+      @Test
+      public void testGetUsersByNameEndpoint() throws Exception {
+            when(userService.getByName("John"))
+                        .thenReturn(Collections.singletonList(
+                                    new User(1, "John Doe", 30)));
+
+            mvc.perform(get("/users/name")
+                        .param("name", "John"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+
+            verify(userService).getByName("John");
+      }
+
 }
