2bd1f8c6cce7f1fb4f11b4ea31d0695b5a6b2d65
Ioana Marica <ioanamarica66@gmail.com>
2026-07-28 11:21:49 +0200

    Introduce and test swagger

--- spring-boot/src/main/java/com/example/spring_project/DemoApplication.java
+++ spring-boot/src/main/java/com/example/spring_project/DemoApplication.java
@@ -3,6 +3,17 @@ package com.example.spring_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+ 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User API",
+                version = "1.0",
+                description = "API documentation for managing users"
+        )
+)
+
 @SpringBootApplication
 public class DemoApplication {
 
