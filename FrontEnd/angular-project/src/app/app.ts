fec800d941500f3403e1d73e5a753c46e8f6fec1
Ioana Marica <ioanamarica66@gmail.com>
2026-07-30 10:34:48 +0200

    Use RxJS and HttpClient to fetch dog images

--- FrontEnd/angular-project/src/app/app.ts
+++ FrontEnd/angular-project/src/app/app.ts
@@ -1,15 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { forkJoin, Observable } from 'rxjs';
+
+interface Dog {
+  message: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [MatButton, MatToolbarModule, MatIconModule, RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatButton, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('my-app');
+
+  dogs$!: Observable<Dog[]>;
+
+  constructor(private http: HttpClient) {}
+
+  fetchDog() {
+    this.dogs$ = forkJoin([
+      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
+      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
+      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
+    ]);
+  }
+
+  ngOnInit(): void {
+    this.fetchDog();
+  }
 }
