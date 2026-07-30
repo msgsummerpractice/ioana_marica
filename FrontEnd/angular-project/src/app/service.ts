d094cb874c135a83e85604b9222b0071a84f1167
Ioana Marica <ioanamarica66@gmail.com>
2026-07-30 12:23:37 +0200

    Remove subscribe from signal implementation

--- FrontEnd/angular-project/src/app/service.ts
+++ FrontEnd/angular-project/src/app/service.ts
@@ -1,25 +1,22 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { forkJoin } from 'rxjs';
 
 export interface Dog {
-  message: string;
+  message: string[];
   status: string;
 }
 
+@Injectable({
+  providedIn: 'root',
+})
 export class Service {
   constructor(private http: HttpClient) {}
 
-  dogs = signal<Dog[]>([]);
+  dogs = signal<string[]>([]);
 
-  fetchDogs(): void {
-    forkJoin([
-      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
-      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
-      this.http.get<Dog>('https://dog.ceo/api/breeds/image/random'),
-    ]).subscribe({
-      next: (dogs) => this.dogs.set(dogs),
-      error: (err) => console.error(err),
-    });
+  async fetchDogs() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random/3');
+    const dog: Dog = await response.json();
+    this.dogs.set(dog.message);
   }
 }
