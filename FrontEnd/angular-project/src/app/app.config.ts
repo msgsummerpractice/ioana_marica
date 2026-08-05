9f1a383a1a7dbb14a71fb35b3933c33b4afb5128
Ioana Marica <ioanamarica66@gmail.com>
2026-08-05 11:56:07 +0200

    Routing woked only localy in angular, trying to make it work in static app

--- FrontEnd/angular-project/src/app/app.config.ts
+++ FrontEnd/angular-project/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './login/auth-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
