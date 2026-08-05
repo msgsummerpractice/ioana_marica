a7eb4629598efb985abdad5ce429684f5e50887d
Ioana Marica <ioanamarica66@gmail.com>
2026-08-05 13:37:58 +0200

    remove import

--- FrontEnd/angular-project/src/app/app.config.ts
+++ FrontEnd/angular-project/src/app/app.config.ts
@@ -1,12 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './login/auth-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-    provideHttpClient(withInterceptors([authInterceptor])),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))],
 };
